19529b34dd996df6017b6a17e41d712515897558
George-Okumu <gokumu12@gmail.com>
2024-07-12 07:50:38 +0300

    feat: mark as purchased

--- index.js
+++ index.js
@@ -6,27 +6,30 @@ const shoppingList = [
         "purchased": false
     },
     {
-        "name": "salt",
-        "price": 15,
-        "image": "https://imgs.search.brave.com/gZBirXfoN1or6jkaNpjUJ9mOblNt2rnk0UpD58dNBMw/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9jZG4u/bWFmcnNlcnZpY2Vz/LmNvbS9zeXMtbWFz/dGVyLXJvb3QvaDA3/L2g0ZS80NzUxMTEw/NTY2NzEwMi8zNzQ5/Ml9tYWluLmpwZz9p/bT1SZXNpemU9NDgw",
+        "name": "Tea",
+        "price": 250,
+        "image": "https://imgs.search.brave.com/fOvWAT__fFR7SrgcNYdC_BLsIrwkxjmTb3ABLbDiVgQ/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9pbWFn/ZXMtbmEuc3NsLWlt/YWdlcy1hbWF6b24u/Y29tL2ltYWdlcy9J/LzcxQlZsQWZ1WE1M/LmpwZw",
         "purchased": false
     }
 ]
+
+display();
+
 
 function display() {
     let div_for_items = document.getElementById('card-list');
 
     div_for_items.innerHTML = '';
 
-    shoppingList.forEach((el) => {
+    shoppingList.forEach((el, index) => {
         div_for_items.innerHTML += `
     
 <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
         <img class="rounded-t-lg" src="${el.image}" alt="item image" />
     <div class="p-5">
             <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">${el.name}</h5>
-        <p class="mb-3 font-normal text-white dark:text-gray-400 bg-blue-500 text-center w-1/2 rounded">Price: ${el.price}</p>
-        <button class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+        <p  id="card-p-${index}" class="mb-3 font-normal text-white dark:text-gray-400 bg-blue-500 text-center w-1/2 rounded">Price: ${el.price}</p>
+        <button id="card-button-${index}" onclick="mark_purchased(${index})" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
             Mark Purchased
         </button>
     </div>
@@ -62,4 +65,19 @@ function addItem(event) {
 
 }
 
-display();
+
+function mark_purchased(index) {
+    shoppingList[index].purchased = true;
+
+
+    let card_body = document.getElementById(`card-p-${index}`);
+    card_body.style = `
+        background-color: red;
+    `
+    let btn = document.getElementById(`card-button-${index}`);
+
+    btn.style.backgroundColor = "red";
+    btn.innerText = 'Purchased';
+
+
+}
